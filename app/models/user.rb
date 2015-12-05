2602f4ca53790278e5bb475b768d7f78d5853072
Nalipp <nateplipp@gmail.com>
2015-12-05 10:59:07 +0900

    user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 8, maximum: 60}
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(email: "nate@gmail.com", password: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "       "
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 106
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = "        "
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short" do
+    @user.password = "a" * 7
+    assert_not @user.valid?
+  end
+
+  test "password should not be too long" do
+    @user.password = "a" * 61
+    assert_not @user.valid?
+  end
 end
